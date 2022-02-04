949bab201a304110ed1c88981381415b6a585ff8
Shanon <shanon.richet@hotmail.com>
2022-02-04 13:22:45 +0100

    move server into functions folder

--- server.mjs
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-const app = express()
-const port = process.env.port || 3000
-
-app.get('/', (req, res) => res.send('Hello '))
-
-app.listen(port, () => console.log('Example'))
